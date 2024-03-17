abca0f149a1baac3d766a0613f8f3406d166c269
sandy1502 <149914839+sandy1502@users.noreply.github.com>
2024-03-16 19:10:28 -0700

    Update code.cc

--- code.cc
+++ code.cc
@@ -21,15 +21,15 @@ if(conditional_check)
 	{
 		case 0:
 		case 1:
-				prediction = (local_prediction[local_pred_indx] > 3);
-				break;
+			prediction = (local_prediction[local_pred_indx] > 3);
+			break;
 		case 2:
 		case 3:
-				prediction = (global_prediction[path_hist_value] > 1);
-				break;
+			prediction = (global_prediction[path_hist_value] > 1);
+			break;
 		default:
-				prediction = true;
-				break;
+			prediction = true;
+			break;
 	}
 }
 else 
@@ -40,42 +40,40 @@ return prediction;
 }
 
 
-
 void PREDICTOR::update_predictor(const branch_record_c* br, const op_state_c* os, bool taken) 
 {
-uint16_t pc 			  = (br->instruction_addr >> 2) % 1024;
+uint16_t pc 		  = (br->instruction_addr >> 2) % 1024;
 uint16_t local_pred_indx  = local_history[pc] % 1024;
 uint16_t path_hist_value  = path_history % 4096;
 bool conditional_check	  = br->is_conditional;
-bool local_pred 		  = (local_prediction[local_pred_indx] > 3);
+bool local_pred 	  = (local_prediction[local_pred_indx] > 3);
 bool global_pred          = (global_prediction[path_hist_value] > 1);
     
 if(conditional_check)
 {
-	path_history = (path_history << 1) | taken;
-    if (global_pred == taken && local_pred != taken)
-	{
-        choice_prediction[path_hist_value] += (choice_prediction[path_hist_value] < 3);
-    } 
-	else if (global_pred != taken && local_pred == taken)
-	{
+path_history = (path_history << 1) | taken;
+if (global_pred == taken && local_pred != taken)
+{
+	choice_prediction[path_hist_value] += (choice_prediction[path_hist_value] < 3);
+} 
+else if (global_pred != taken && local_pred == taken)
+{
         choice_prediction[path_hist_value] -= (choice_prediction[path_hist_value] > 0);
-    }
-    if (taken)
-	{
-            global_prediction[path_hist_value]  +=(global_prediction[path_hist_value] < 3);
-            local_prediction[local_pred_indx]	+=(local_prediction[local_pred_indx] < 7);
-    } 
-	else
-	{
-            global_prediction[path_hist_value]  -=(global_prediction[path_hist_value] > 0);	
-            local_prediction[local_pred_indx]	-=(local_prediction[local_pred_indx] > 0);
-    }
-	local_history[pc] = (local_history[pc] << 1) | taken;
-
+}
+if (taken)
+{
+        global_prediction[path_hist_value]  +=(global_prediction[path_hist_value] < 3);
+	local_prediction[local_pred_indx]	+=(local_prediction[local_pred_indx] < 7);
+} 
+else
+{
+	global_prediction[path_hist_value]  -=(global_prediction[path_hist_value] > 0);	
+        local_prediction[local_pred_indx]	-=(local_prediction[local_pred_indx] > 0);
+}
+local_history[pc] = (local_history[pc] << 1) | taken;
 }
 else
 {
-    path_history = (path_history << 1) | taken;
+	path_history = (path_history << 1) | taken;
 }
 }
